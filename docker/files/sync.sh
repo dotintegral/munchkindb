571756c39d5497ee03f0898641109bffaf50d37a
Artur Siery <dotintegral@gmail.com>
2018-03-30 19:39:16 +0200

    Fixed command line error in sync.sh

--- docker/files/sync.sh
+++ docker/files/sync.sh
@@ -5,7 +5,7 @@ do
   sleep 1
   rsync -avr \
   --no-perms --no-group --no-owner \
-  --exclude 'app/config/parameters.yml'
-  --exclude '.htaccess'
+  --exclude 'app/config/parameters.yml' \
+  --exclude '.htaccess' \
   /source/ /var/www/html/
 done
